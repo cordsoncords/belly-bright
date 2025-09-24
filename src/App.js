c2a6bd3758a60d565bd2382cedb1d1f82dcab9ec
Cordero Miguel <corderomiguel@Corderos-Laptop.local>
2025-09-23 20:35:32 -0400

    Add Facebook Pixel, Google Analytics, and Microsoft Clarity

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // src/App.js
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -15,10 +16,61 @@ import ServiceAreas from "./pages/ServiceAreas";
 import City from "./pages/City";
 import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact";
-import FAQ from "./pages/FAQ"
+import FAQ from "./pages/FAQ";
 import NotFound from "./pages/NotFound";
 
 export default function App() {
+  useEffect(() => {
+    // Facebook Pixel
+    !(function (f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function () {
+        n.callMethod
+          ? n.callMethod.apply(n, arguments)
+          : n.queue.push(arguments);
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = true;
+      n.version = "2.0";
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = true;
+      t.src = "https://connect.facebook.net/en_US/fbevents.js";
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    })(window, document, "script");
+
+    window.fbq("init", "1078650917748817");
+    window.fbq("track", "PageView");
+
+    // Google Analytics
+    const gtagScript = document.createElement("script");
+    gtagScript.setAttribute("async", "");
+    gtagScript.src =
+      "https://www.googletagmanager.com/gtag/js?id=G-HSYFZK4KYE";
+    document.head.appendChild(gtagScript);
+
+    const gtagInit = document.createElement("script");
+    gtagInit.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-HSYFZK4KYE');
+    `;
+    document.head.appendChild(gtagInit);
+
+    // Microsoft Clarity
+    const clarityScript = document.createElement("script");
+    clarityScript.type = "text/javascript";
+    clarityScript.innerHTML = `(function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "tfdgoj53kj");`;
+    document.head.appendChild(clarityScript);
+  }, []);
+
   return (
     <BrowserRouter>
       <ScrollToTop />
